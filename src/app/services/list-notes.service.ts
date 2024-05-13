d174b8e7e6d7f8af3024554790a156458d69630c
jsindol-ganda <juanloven.m.sindol@lpstech.com>
2024-05-13 13:28:37 +0800

    Modify service call

--- src/app/services/list-notes.service.ts
+++ src/app/services/list-notes.service.ts
@@ -3,37 +3,47 @@ import { Injectable } from '@angular/core';
 import { Observable, catchError } from 'rxjs';
 import { Note } from '../models/Note';
 import { NoteResponse } from '../models/NoteResponse';
-import { environment } from '../../environments/environment';
+// import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ListNotesService {
-  url = environment.apiURL;
+  // url = environment.apiURL;
 
   constructor(private httpClient: HttpClient) { }
 
   getAllNotes(): Observable<Array<Note>> {
-    return this.httpClient.get<Array<Note>>(`${this.url}/api/getAllNotes`);
+    // return this.httpClient.get<Array<Note>>(`${this.url}/api/getAllNotes`);
+    return this.httpClient.get<Array<Note>>(`/api/getAllNotes`);
   }
 
   getANote(id: number): Observable<Note> {
-    return this.httpClient.post<Note>(`${this.url}/api/getNote`, {id})
-      .pipe(catchError(error => {throw new Error(error.message)}));
+    // return this.httpClient.post<Note>(`${this.url}/api/getNote`, {id})
+    //   .pipe(catchError(error => {throw new Error(error.message)}));
+    return this.httpClient.post<Note>(`/api/getNote`, {id})
+    .pipe(catchError(error => {throw new Error(error.message)}));
   }
 
   addNote(note: Note): Observable<NoteResponse> {
-    return this.httpClient.post<NoteResponse>(`${this.url}/api/addNote`, note)
+    // return this.httpClient.post<NoteResponse>(`${this.url}/api/addNote`, note)
+    // .pipe(catchError(error => {console.log(error); throw new Error(error.message)}));
+    return this.httpClient.post<NoteResponse>(`/api/addNote`, note)
     .pipe(catchError(error => {console.log(error); throw new Error(error.message)}));
   }
 
   updateNote(note: Note): Observable<NoteResponse> {
-    return this.httpClient.put<NoteResponse>(`${this.url}/api/updateNote`, note)
+    // return this.httpClient.put<NoteResponse>(`${this.url}/api/updateNote`, note)
+    // .pipe(catchError(error => {throw new Error(error.message)}));
+    return this.httpClient.put<NoteResponse>(`/api/updateNote`, note)
     .pipe(catchError(error => {throw new Error(error.message)}));
   }
 
   deleteNote(id: number): Observable<NoteResponse> {
-    return this.httpClient.delete<NoteResponse>(`${this.url}/api/deleteNote`, {
+    // return this.httpClient.delete<NoteResponse>(`${this.url}/api/deleteNote`, {
+    //   body: {id}
+    // }).pipe(catchError(error => {throw new Error(error.message)}));
+    return this.httpClient.delete<NoteResponse>(`/api/deleteNote`, {
       body: {id}
     }).pipe(catchError(error => {throw new Error(error.message)}));
   }
